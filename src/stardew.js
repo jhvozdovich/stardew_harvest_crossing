f043e2e191e42b1a5748d0fa88db1b0fc1068601
brevin <cronkbrevin98@gmail.com>
2020-04-28 13:30:42 -0700

    commit

--- src/stardew.js
+++ src/stardew.js
@@ -1,9 +1,21 @@
 export class Farm {
   constructor(farmName){
     this.farmName = farmName,
-    this.cropOne = 0;
-    this.cropTwo = 0;
-    this.cropThree = 0;
+    this.cropOne = {
+      alive=true,
+      water: 5,
+      age: 0
+    }
+    this.cropTwo =  {
+      alive=true,
+      water: 5,
+      age: 0
+    }
+    this.cropThree =  {
+      alive=true,
+      water: 5,
+      age:0
+    }
     this.animalOne= {
       health: 15,
       hunger: 15
@@ -19,11 +31,49 @@ export class Farm {
   }
 }
 
+function waterCrop(){
+  this.water += 5;
+  if(this.water >= 25){
+    this.water = 25;
+  }if (this.water <= 0){
+    alive = false;
+  }
+}
+
+function increasePlantAge(){
+  setInterval(() => {
+    this.age++
+    if(this.age = 2){
+      //harvest function
+    }
+    if(this.age > 3){
+      alive=false;
+    }
+    
+  },300000)
+}
+
+function harvest(){
+  if(this.cropOne.age == 2 || this.cropTwo.age == 2 || this.cropThree.age == 2){
+    //push to array
+
+  }
+  else{
+    //message saying crops arent ready yet
+  }
+}
+
+
+
+
+
+
 export class Character {
   constructor(charName){
     this.charName = charName;
     this.isAlive = true;
     this.xp= 0;
+    this.inventory=[]
     this.stats = {
       health: 100,
       currency: 1500,
