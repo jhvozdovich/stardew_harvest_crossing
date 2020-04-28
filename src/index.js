b0f08a8a463ae7f55784dca6551a8bc394fe43e2
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-28 15:23:04 -0700

    add player sprite and animation motion logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,85 @@
 import Phaser from "phaser";
 import farmbg from "./assets/background-image.png";
-import witch from "./assets/farmer.png";
+import witch from "./assets/farmer-walk.png";
+
+const gameState = {}
+
+
+
+function preload() {
+  this.load.image("farmBackground", farmbg);
+  this.load.spritesheet("witch", witch, { frameWidth: 62, frameHeight: 83});
+}
+
+function create() {
+  let background = this.add.image(300, 300, "farmBackground");
+  background.setScale(.5);
+  gameState.witchSprite = this.add.sprite(150, 150, "witch");
+  gameState.cursors = this.input.keyboard.createCursorKeys();
+
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("witch", {start: 0, end: 2}),
+    frameRate: 5,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: "down",
+    frames: this.anims.generateFrameNumbers("witch", {start: 3, end: 5}),
+    frameRate: 5,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: "up",
+    frames: this.anims.generateFrameNumbers("witch", {start: 6, end: 8}),
+    frameRate: 5 ,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("witch", {start: 9, end: 11}),
+    frameRate: 5,
+    repeat: -1
+  });
+
+}
+
+function update () {
+  if (gameState.cursors.right.isDown && gameState.cursors.up.isDown) {
+    gameState.witchSprite.x +=3;
+    gameState.witchSprite.y -=3;
+    gameState.witchSprite.anims.play('right', true);
+  } else if (gameState.cursors.right.isDown && gameState.cursors.down.isDown) {
+    gameState.witchSprite.x +=3;
+    gameState.witchSprite.y +=3;
+    gameState.witchSprite.anims.play('right', true);
+  } else if (gameState.cursors.left.isDown && gameState.cursors.up.isDown) {
+    gameState.witchSprite.x -=3;
+    gameState.witchSprite.y -=3;
+    gameState.witchSprite.anims.play('left', true);
+  } else if (gameState.cursors.left.isDown && gameState.cursors.down.isDown) {
+    gameState.witchSprite.x -=3;
+    gameState.witchSprite.y +=3;
+    gameState.witchSprite.anims.play('left', true);
+  } else if (gameState.cursors.right.isDown) {
+    gameState.witchSprite.x +=3;
+    gameState.witchSprite.anims.play('right', true);
+  } else if (gameState.cursors.left.isDown) {
+    gameState.witchSprite.x -=3;
+    gameState.witchSprite.anims.play('left', true);
+  } else if (gameState.cursors.down.isDown) {
+    gameState.witchSprite.y +=3;
+    gameState.witchSprite.anims.play('down', true);
+  } else if (gameState.cursors.up.isDown) {
+    gameState.witchSprite.y -=3;
+    gameState.witchSprite.anims.play('up', true);
+  } else {
+    gameState.witchSprite.anims.stop();
+  }
+}
 
 const config = {
   type: Phaser.AUTO,
@@ -14,24 +93,33 @@ const config = {
   }
 };
 
-let witchSprite
-
 const game = new Phaser.Game(config);
 
-function preload() {
-  this.load.image("farmBackground", farmbg);
-  this.load.image("witch", witch);
-}
 
-function create() {
-  let background = this.add.image(300, 300, "farmBackground");
-  background.setScale(.5);
-  witchSprite = this.add.sprite(20, 30, "witch");
-}
 
-function update () {
-  witchSprite.x += 1;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/stardew.js
+++ src/stardew.js
@@ -1,6 +1,108 @@
 export class Farm {
   constructor(farmName){
     this.farmName = farmName,
+    this.cropOne = {
+      alive=true,
+      water: 5,
+      age: 0
+    }
+    this.cropTwo =  {
+      alive=true,
+      water: 5,
+      age: 0
+    }
+    this.cropThree =  {
+      alive=true,
+      water: 5,
+      age:0
+    }
+    this.animalOne= {
+      health: 15,
+      hunger: 15
+    };
+    this.animalTwo = {
+      health: 15,
+      hunger: 15
+    };
+    this.animalThree = {
+      health: 15,
+      hunger: 15
+    };
+  }
+}
+
+function waterCrop(){
+  this.water += 5;
+  if(this.water >= 25){
+    this.water = 25;
+  }if (this.water <= 0){
+    alive = false;
+  }
+}
+
+function increasePlantAge(){
+  setInterval(() => {
+    this.age++
+    if(this.age = 2){
+      //harvest function
+    }
+    if(this.age > 3){
+      alive=false;
+    }
+    
+  },300000)
+}
+
+function harvest(){
+  if(this.cropOne.age == 2 || this.cropTwo.age == 2 || this.cropThree.age == 2){
+    //push to array
+
+  }
+  else{
+    //message saying crops arent ready yet
+  }
+}
+
+
+
+
+
+
+export class Character {
+  constructor(charName){
+    this.charName = charName;
+    this.isAlive = true;
+    this.xp= 0;
+    this.inventory=[]
+    this.stats = {
+      health: 100,
+      currency: 1500,
+      level: 1,
+      hunger: 0,
+      age: 0
+    };
+  }
+}
+
+export class Companion {
+  constructor(petName){
+    this.petName = petName;
+    this.age = 0;
+    this.health = 50;
+    this.happy = 25;
+    this.bathroom = 0;
+    this.hungry = 0;
+  }
+}
+
+
+
+
+
+
+export class Farm {
+  constructor(farmName){
+    this.farmName = farmName,
     this.cropOne = 0;
     this.cropTwo = 0;
     this.cropThree = 0;
